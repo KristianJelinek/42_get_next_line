2a3e229e541fd7819b01b6dd8965e365ed0b7907
Kristian Jelinek <kjelinek@c2r10s1.42prague.com>
2023-12-07 16:28:04 +0100

    gnl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: kjelinek < kjelinek@student.42prague.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:50:32 by k                 #+#    #+#             */
-/*   Updated: 2023/12/04 16:36:21 by kjelinek         ###   ########.fr       */
+/*   Updated: 2023/12/07 16:09:34 by kjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,84 +14,100 @@
 
 char	*ft_strdup(const char *s)
 {
-	char	*rtn;
-	size_t	len;
+	size_t		len;
+	char		*dst;
+	const char	*src;
+	char		*rtn;
 
-	len = ft_strlen(s) + 1;
-	rtn = malloc(sizeof(char) * len);
+	if (!s)
+		return (NULL);
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	*rtn = (char *)malloc(sizeof(char) * (len + 1));
 	if (!rtn)
-		return (0);
-	rtn = ft_memcpy(rtn, s, len);
+		return (NULL);
+	dst = rtn;
+	*src = s;
+	while (*src != '\0')
+		*dst++ = *src++;
+	*dst = '\0';
 	return (rtn);
 }
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	size_t	lenght_s1;
-	size_t	lenght_s2;
-	size_t	total_s_lenght;
+	char	*dst;
 	char	*rtn;
+	size_t	len1;
+	size_t	len2;
 
 	if (!s1 && !s2)
 		return (ft_strdup(""));
-	if (s1 && !s2)
-		return (ft_strdup(s1));
-	if (!s1 && s2)
-		return (ft_strdup(s2));
-	lenght_s1 = ft_strlen((char *)s1);
-	lenght_s2 = ft_strlen(s2);
-	total_s_lenght = lenght_s1 + lenght_s2 + 1;
-	rtn = malloc(sizeof(char) * total_s_lenght);
+	len1 = (s1) ? ft_strlen(s1) : 0;
+	len2 = (s2) ? ft_strlen(s2) : 0;
+	*rtn = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!rtn)
-		return (0);
-	ft_memmove(rtn, s1, lenght_s1);
-	ft_memmove(rtn + lenght_s1, s2, lenght_s2);
-	rtn[total_s_lenght - 1] = '\0';
+		return (NULL);
+	*dst = rtn;
+	if (s1)
+	{
+		while (*s1 != '\0')
+			*dst++ = *s1++;
+	}
+	if (s2)
+	{
+		while (*s2 != '\0')
+			*dst++ = *s2++;
+	}
+	*dst = '\0';
 	return (rtn);
 }
 
-char	*ft_strchr(const char *s, int i)
+char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-	{
-		if (*s == (char)i)
-			return ((char *)s);
+	while (*s != '\0' && *s != (char)c)
 		s++;
-	}
-	if (i == '\0')
-		return ((char *)s);
-	return (0);
+	return (*s == (char)c) ? (char *)s : NULL;
 }
 
 size_t	ft_strlen(const char *str)
 {
-	size_t	i;
+	const char	*s = str;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+	while (*s != '\0')
+		s++;
+	return (s - str);
 }
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	size_t	s_len;
-	size_t	sub_len;
-	char	*str;
+	char		*dst;
+	const char	*src;
+	size_t		sub_len;
+	char		*str;
+	size_t		s_len;
 
 	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_strdup(""));
-	sub_len = len;
 	if (len >= s_len - start)
+	{
 		sub_len = s_len - start;
-	str = (char *)malloc(sub_len + 1);
+	}
+	else
+	{
+		sub_len = len;
+	}
+	*str = (char *)malloc(sub_len + 1);
 	if (!str)
-		return (NULL);
-	str[sub_len] = '\0';
-	while (sub_len--)
-		str[sub_len] = s[start + sub_len];
+		*dst = str;
+	*src = s + start;
+	return (NULL);
+	while (sub_len-- > 0)
+		*dst++ = *src++;
+	*dst = '\0';
 	return (str);
 }
